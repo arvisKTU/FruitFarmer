a6ba83bfc25981bdd61473f630892230718829f9
DESKTOP-E4U69SA\Arvis <arvis12345@gmail.com>
2020-11-25 19:11:22 +0200

    Many fixes
    fixed progress bar, UI for different resolutions, added object pooling and other fixes

---
+++ Assets/Scripts/FruitDragAndDrop.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitDragAndDrop : MonoBehaviour
+{
+    private const string PUT_INTO_BASKET_CLIP_NAME = "put_into_basket";
+    public bool isDragging;
+    
+    public GameObject correctBasket;
+    private Vector2 initialPos;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public float distanceWithinBasket;
+
+    public FillProgressBar progressBar;
+    public FruitGenerator generator;
+    public GameManager gameManager;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        initialPos = transform.position;
+        mainCamera = Camera.main;
+    }
+    private void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = mainCamera.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+        }
+    }
+    private void OnMouseDown()
+    {
+        if (!gameManager.gameEnded)
+        {
+            isDragging = true;
+            screenPoint = mainCamera.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        if (Mathf.Abs(transform.position.x - correctBasket.transform.position.x) <= distanceWithinBasket &&
+            (Mathf.Abs(transform.position.y - correctBasket.transform.position.y) <= distanceWithinBasket))
+        {
+            SoundManager.PlaySound(PUT_INTO_BASKET_CLIP_NAME);
+            generator.CountDecrease();
+            progressBar.AddFruit();
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.position = initialPos;
+        }
+    }
+}
---
+++ Assets/Scripts/FruitPooler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitPooler : MonoBehaviour
+{
+
+    public GameObject pooledFruit;
+
+    public int pooledAmount;
+
+    List<GameObject> pooledFruits;
+    void Start()
+    {
+        pooledFruits = new List<GameObject>();
+
+        for (int i = 0; i < pooledAmount; i++)
+        {
+
+
+            GameObject fruit = (GameObject)Instantiate(pooledFruit, new Vector3(0, 0, 0), Quaternion.identity);
+            fruit.SetActive(false);
+            pooledFruits.Add(fruit);
+        }
+    }
+    public GameObject GetPooledFruit()
+    {
+        for (int i = 0; i < pooledFruits.Count; i++)
+        {
+            if (!pooledFruits[i].activeInHierarchy)
+            {
+                return pooledFruits[i];
+            }
+        }
+
+        //GameObject fruit = (GameObject)Instantiate(pooledFruit, FindSpawnPosition(), Quaternion.identity);
+        //fruit.SetActive(false);
+        //pooledFruits.Add(fruit);
+        return null;
+    }
+}
